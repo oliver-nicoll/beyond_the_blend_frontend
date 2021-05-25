8bcc74555a74a255340e27fd4fdd92f7c5a59dfb
Nicoll Oliver <nicolldon96@gmail.com>
2021-05-24 23:02:29 -0400

    marketAdapter

--- src/components/marketAdapter.js
+++ src/components/marketAdapter.js
@@ -1,1 +1,98 @@
-// fetch request to back end
+class ShoppeAdapter {
+
+    constructor(baseURL){
+        this.basemarketURL = `${baseURL}/api/v1/markets`
+    }
+
+    getmarkets(){
+        fetch(this.basemarketURL)
+        .then(r => r.json())
+        .then(market => {
+            market.forEach(market => {
+                const m = new Market(market)
+                m.addToDom()
+            })
+        })
+        .catch(error => console.error(error))
+
+
+        // function fetchmarkets(){
+        //     // const marketsContainer = document.getElementById("markets-container")
+        
+        //     // fetch("http://localhost:3000/api/v1/markets")
+        //     // .then(r => r.json())
+        //     // .then(data => {
+        //     //     data.forEach(addmarket)
+        //     // })
+        //     // .catch(err => console.warn(err))
+        // }
+    }
+
+    editMarket(editMode, nameInput){
+        fetch(`http://127.0.0.1:3000/api/v1/markets/${editMode.dataset.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                name: nameInput.value
+            })
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            if (data.status === 204) {
+                // editMode.children[0].innerText = data.market.name
+                // editMode = false
+                // document.getElementById('market-submit').value = "Create market"
+                // nameInput.value = ""  
+            } else {
+                alert(data.errors)
+            }
+        })
+        .catch(err => console.error(err))
+    }
+
+    createMarket(nameInput){
+        fetch("http://127.0.0.1:3000/api/v1/markets", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                name: nameInput.value
+            })   
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            console.log("I'm in the second then!", data)
+            if (data.status === 201){
+                const s = new market(data.market)
+                s.addToDom()
+            } else {
+                alert(data.errors)
+            }
+            nameInput.value = ""
+        })
+        .catch(err => console.error("I'm in the catch!", err))
+    }
+
+    deleteMarket(li){
+        fetch(`http://127.0.0.1:3000/api/v1/markets/${li.dataset.id}`, {
+            method: "DELETE"
+        })
+        .then(resp => {
+            console.log(resp)
+            return resp.json()
+        })
+        .then(data => {
+            if (data.message === "Successfully deleted"){
+                li.remove()
+            } else {
+                alert(data.message)
+            }
+        })
+        .catch(err => console.error(err))
+    }
+}
